ca597a86cb0b1c8cb31b68e32e8b2baec7cc9e9c
dotun-code-god <dotun494@gmail.com>
2023-08-19 17:55:21 +0100

    added question as comment

--- php-array-test.php
+++ php-array-test.php
@@ -1,4 +1,19 @@
 <?php
+/**
+     Write a function to that will sort the object below in ascending order based on id.
+    Note that the id can be any of string or number
+    The function will take an array as an argument and return a new array based on the instruction above.
+
+    const arrayObj = [
+    {id: '4', name: 'kelvin Table'},
+    {id: '3', name: 'kelvin Chair'},
+    {id: '7', name: 'Peter Table'},
+    {id: '4', name: 'kelvin Table Oliver'}
+    {id: 4, name: 'kelvin Oliver'}
+    {id: '8', name: 'Mary Steve'}
+    ]
+ */
+
 
 function arraySort($arr) {
   usort($arr, function($a, $b) {
